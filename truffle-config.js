fdd5ea6d59e5a04ddbccbdd0ac7953c4d8dcb744
cdyfng <xiaofeng_chen2007@163.com>
2020-06-13 07:32:46 +0800

    fit local ganache

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,8 @@
 var HDWalletProvider = require("truffle-hdwallet-provider");
 const mnemonic = "";
 const walletChildNum = 0;
-const networkAddress = "https://mainnet.infura.io/v3/<your-api-key>";
+// const networkAddress = "https://mainnet.infura.io/v3/<your-api-key>";
+const networkAddress = "https://mainnet.infura.io/v3/" + process.env.INFURA_API_KEY;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -9,7 +10,7 @@ module.exports = {
   networks: {
     development: {
       host: '127.0.0.1',
-      port: 8545, // ganache-cli
+      port: 7545, // ganache-cli
       network_id: '*', // Match any network id
       gas: 6700000,
       gasPrice: 0x01
